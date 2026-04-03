74de6e9451f4e5b65a6f1838019a1c68d1c4fc35
manjariii8 <manji9452@gmail.com>
2026-04-03 15:53:07 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+/*************************toggle icon navbar******************* */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+/*************************toggle icon navbar******************* */
+let section = document.querySelector('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll= () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach.apply(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        }
+    });
+    /*****Sticky navbar************/
+    let header = document.querySelector('.header');
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+    /*******Close menu on scroll**** */
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+/********scroll reveal********/
+ScrollReveal({
+    distance:'60px',
+    duration: 2000,
+    delay: 200,
+});
+ScrollReveal().reveal('.home-content, .heading',{origin:'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact-form',{origin:'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img',{origin:'left'});
+ScrollReveal().reveal('.home-content p, .about-content',{origin:'right'});
+
+/********typed reveal********/
+const typed = new Typed('.multiple-text',{
+    strings: ['An aspiring Software Developer','Java Developer','Frontend Developer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDealy:1000,
+    loop: true,
+});
+
+
+document.querySelectorAll('.services-box .btn').forEach(btn => {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        alert("More details coming soon!");
+    });
+});
